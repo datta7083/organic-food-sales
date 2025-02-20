72e2f13a36870528dc931ca5e4e692dc27967d2e
datta7083 <dattagulakhe93@gmail.com>
2025-02-20 11:56:47 +0530

    Delete organic_food_sales directory

--- organic_food_sales/test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once('config.php');
-if ($conn) {
-    echo "Database connected successfully!";
-} else {
-    echo "Failed to connect to the database.";
-}
-?>
--- organic_food_sales/vegitables/script1.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const navbarToggler = document.querySelector(".navbar-toggler");
-    const navbarMenu = document.querySelector("#navbarNav");
-
-    navbarToggler.addEventListener("click", function () {
-        navbarMenu.classList.toggle("show");
-    });
-});
